b4d956fe7cf8941dd52b6a0c544adc1d7180a550
icetec <pit@ice-technologies.de>
2016-06-10 07:28:10 +0200

    First try

---
+++ Scripts/ICECreatureORKAdapter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using ORKFramework;
+using ORKFramework.Behaviours;
+
+public class ICECreatureORKAdapter : DamageZone {
+
+	// Use this for initialization
+	void Start () {
+	
+		base.Combatant.d
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
